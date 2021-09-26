ede539014eef9beb89a2f1e187c9d37f0371cac3
Alexandre Franca <basic4@gmail.com>
2021-09-26 09:55:46 +0100

    first commit

---
+++ src/main/java/com/example/s3app/ListObjects.java
@@ -0,0 +1,32 @@
+package com.example.s3app;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import java.util.List;
+
+public class ListObjects {
+    public static void main(String[] args) {
+        final String USAGE = "\n" +
+                "To run this example, supply the name of a bucket to list!\n" +
+                "\n" +
+                "Ex: ListObjects <bucket-name>\n";
+
+        if (args.length < 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String bucket_name = args[0];
+
+        System.out.format("Objects in S3 bucket %s:\n", bucket_name);
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        ListObjectsV2Result result = s3.listObjectsV2(bucket_name);
+        List<S3ObjectSummary> objects = result.getObjectSummaries();
+        for (S3ObjectSummary os : objects) {
+            System.out.println("* " + os.getKey());
+        }
+    }
+}
