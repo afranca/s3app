f35e9c1feb39a3c3ad7d74525ee845f6b5a44482
Alexandre Franca <basic4@gmail.com>
2021-10-13 15:13:16 +0100

    feat: refactoring code

--- src/main/java/com/example/s3app/S3Versioning.java
+++ src/main/java/com/example/s3app/S3Versioning.java
@@ -27,40 +27,50 @@ public class S3Versioning {
 
     
 	private static void goThroughVersionsInBucket(AmazonS3 s3,String bucketName) {
-		ListVersionsRequest request = new ListVersionsRequest().withBucketName(bucketName);
-		VersionListing versionListing = s3.listVersions(request);
 		
-		List<S3VersionSummary> keyVersions;
 		List<String> keys = listAllKeysForBucket(s3, bucketName);		
 		for (String key : keys) {
-			keyVersions = new ArrayList<S3VersionSummary>();
-			System.out.println("--------------------------------------------");
-			System.out.println(" Key: " + key + "  " );
-	        for (S3VersionSummary objectSummary :  versionListing.getVersionSummaries()) {
-	        	if (key.equalsIgnoreCase(objectSummary.getKey())) {
-	        		System.out.println(" 	- Version: " + objectSummary.getKey() + "  " +
-	                    //"(size = " + objectSummary.getSize() + ")" +
-	                    "(" + objectSummary.getVersionId() + ") "+
-	                    "("+objectSummary.getLastModified()+")"
-	                    );
-	        		keyVersions.add(objectSummary);	
-	        	}
-	        	
-	        }	
-	        Stream<S3VersionSummary> stream = keyVersions.stream();
-	        stream = stream.sorted(Comparator.comparing(S3VersionSummary::getLastModified));
-			List<S3VersionSummary> lst = stream.collect(Collectors.toList());
 			
-			if (lst.size() > 0) {
-				S3VersionSummary firstVersion = lst.get(0);
-				System.out.println("\n>> First Version: " + firstVersion.getKey() + "  " +
-						"(" + firstVersion.getVersionId() + ") "+
-						"("+firstVersion.getLastModified()+") \n" );
-			}
+			S3VersionSummary originalVersion =	retrieveOriginalVersion(s3,bucketName,key);
+			System.out.println("\n>> Original Version: " + originalVersion.getKey() + "  " +
+					"(" + originalVersion.getVersionId() + ") "+
+					"("+originalVersion.getLastModified()+") \n" );
 		
 		}
 		
     }
+
+
+	private static S3VersionSummary retrieveOriginalVersion(AmazonS3 s3,String bucketName,String key) {
+		
+		ListVersionsRequest request = new ListVersionsRequest().withBucketName(bucketName);
+		VersionListing versionListing = s3.listVersions(request);
+		
+		List<S3VersionSummary> keyVersions;
+		keyVersions = new ArrayList<S3VersionSummary>();
+		System.out.println("--------------------------------------------");
+		System.out.println(" Key: " + key + "  " );
+		for (S3VersionSummary objectSummary :  versionListing.getVersionSummaries()) {
+			if (key.equalsIgnoreCase(objectSummary.getKey())) {
+				System.out.println(" 	- Version: " + objectSummary.getKey() + "  " +
+		            //"(size = " + objectSummary.getSize() + ")" +
+		            "(" + objectSummary.getVersionId() + ") "+
+		            "("+objectSummary.getLastModified()+")"
+		            );
+				keyVersions.add(objectSummary);	
+			}
+			
+		}	
+		Stream<S3VersionSummary> stream = keyVersions.stream();
+		stream = stream.sorted(Comparator.comparing(S3VersionSummary::getLastModified));
+		List<S3VersionSummary> lst = stream.collect(Collectors.toList());
+		
+		if (lst.isEmpty()) return null;
+		
+		S3VersionSummary originalVersion = lst.get(0);
+		return originalVersion;
+		
+	}
 
 	private static List<String> listAllKeysForBucket(AmazonS3 s3, String bucketName) {
 		ListObjectsV2Result result = s3.listObjectsV2(bucketName);
